c028a358daa1da6df447d04ff0434c11ec44a1ec
styphyPang <1024572990@qq.com>
2020-05-25 14:37:15 +0800

    "第一次提交" git commit -m "第一次提交" git remote add origin git@https://github.com/STEPHYSSS/JinDu.git git commit -m 第一次提交

---
+++ src/views/right.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="content">d
+  </div>
+</template>
+<script>
+export default {
+    name:'home',
+    data(){
+        return {
+        }
+    }
+}
+</script>
+<style>
+</style>
